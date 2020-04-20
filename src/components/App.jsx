3c4728071f32e2e88c81cb4b762c607c6d5670b9
Chamoda Gamage <35315540+chamodagamage@users.noreply.github.com>
2020-04-20 15:50:44 +1000

    map
    added map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts.js";
 
+function GetContact(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map(contact => (
-        <Card
-          name={contact.name}
-          img={contact.imgURL}
-          tel={contact.phone}
-          email={contact.email}
-        />
-      ))}
+      {contacts.map(GetContact)}
     </div>
   );
 }
